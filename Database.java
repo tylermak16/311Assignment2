fb5aa605a4ea674420aef79ab1bb8980930ae590
tylermak16 <tylermak@hawaii.edu>
2024-06-14 22:06:21 -1000

    db sayings

--- Database.java
+++ Database.java
@@ -10,53 +10,76 @@ import java.util.*;
 
 public class Database {
     TreeSet<Saying> tree;
-    Map<String, List<Saying>> hawaiianToSayings;
+    Map<String, List<Saying>> arabicToSayings;
     Map<String, List<Saying>> englishToSayings;
 
     public Database() {
         tree = new TreeSet<>();
-        hawaiianToSayings = new HashMap<>();
+        arabicToSayings = new HashMap<>();
         englishToSayings = new HashMap<>();
     }
 
-    public boolean Member(Saying s){
+    public boolean Member(Saying s) {
         return tree.contains(s);
     }
 
-    public Saying First(){
+    public Saying First() {
         return tree.first();
     }
 
-    public Saying Last(){
+    public Saying Last() {
         return tree.last();
     }
 
-    public Saying Predecessor(Saying s){
+    public Saying Predecessor(Saying s) {
         return tree.lower(s);
     }
 
-    public Saying Successor(Saying s){
+    public Saying Successor(Saying s) {
         return tree.higher(s);
     }
 
-    public void Insert(Saying s){
-        tree.add(s);
-
-        // Index words for MeHua and WithWord operations
-        for (String word : s.getHawaiian().split(" ")) {
-            hawaiianToSayings.computeIfAbsent(word, k -> new ArrayList<>()).add(s);
+    public void Insert(Saying s) {
+        if (tree.add(s)) {
+            indexSaying(s);
         }
+    }
 
-        for (String word : s.getEnglish().split(" ")) {
+    public List<Saying> MeHua(String word) {
+        return arabicToSayings.getOrDefault(word, Collections.emptyList());
+    }
+
+    public List<Saying> WithWord(String word) {
+        return englishToSayings.getOrDefault(word, Collections.emptyList());
+    }
+
+    private void indexSaying(Saying s) {
+        for (String word : s.getArabic().split("\\s+")) {
+            arabicToSayings.computeIfAbsent(word, k -> new ArrayList<>()).add(s);
+        }
+        for (String word : s.getEnglish().split("\\s+")) {
             englishToSayings.computeIfAbsent(word, k -> new ArrayList<>()).add(s);
         }
     }
 
-    public List<Saying> MeHua(String s){
-        return hawaiianToSayings.getOrDefault(s, new ArrayList<>());
-    }
+    public static void main(String[] args) {
+        Database db = new Database();
 
-    public List<Saying> WithWord(String s){
-        return englishToSayings.getOrDefault(s, new ArrayList<>());
+        Saying s1 = new Saying("الحب لكل واحد", "Love one another", "شرح باللغة العربية", "Explanation in English");
+        Saying s2 = new Saying("اعتني جيدًا", "Take good care", "شرح باللغة العربية", "Explanation in English");
+        Saying s3 = new Saying("الأرض رئيس؛ الرجل خادمها", "The land is a chief; man is its servant", "شرح باللغة العربية", "Explanation in English");
+
+        db.Insert(s1);
+        db.Insert(s2);
+        db.Insert(s3);
+
+        System.out.println("First: " + db.First().getArabic());
+        System.out.println("Last: " + db.Last().getArabic());
+        System.out.println("Predecessor of s2: " + db.Predecessor(s2).getArabic());
+        System.out.println("Successor of s2: " + db.Successor(s2).getArabic());
+
+        System.out.println("Sayings with 'الحب': " + db.MeHua("الحب"));
+        System.out.println("Sayings with 'Love': " + db.WithWord("Love"));
     }
 }
+
--- Saying.java
+++ Saying.java
@@ -4,20 +4,49 @@
  * Tyler Mak
  * Date: 6/14/2024
  */
-public class Saying {
+public class Saying implements Comparable<Saying> {
     String arabicS;
     String englishT;
     String arabicE;
     String englishE;
-    public Saying(String arabicS,String englishT,String arabicE,String englishE){
+
+    public Saying(String arabicS, String englishT, String arabicE, String englishE) {
         this.arabicS = arabicS;
         this.englishT = englishT;
         this.arabicE = arabicE;
         this.englishE = englishE;
     }
 
+    public String getArabic() {
+        return arabicS;
+    }
+
+    public String getEnglish() {
+        return englishT;
+    }
+
+    @Override
     public String toString() {
         return "Arabic: " + arabicS + "\nEnglish: " + englishT +
                 "\nArabic Explanation: " + arabicE + "\nEnglish Explanation: " + englishE;
     }
+
+    @Override
+    public int hashCode() {
+        return arabicS.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Saying saying = (Saying) obj;
+        return arabicS.equals(saying.arabicS);
+    }
+
+    @Override
+    public int compareTo(Saying other) {
+        return this.arabicS.compareTo(other.arabicS);
+    }
 }
+
