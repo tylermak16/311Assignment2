486b4e30659bb4bbd7de3b005307af45de203700
tylermak16 <tylermak@hawaii.edu>
2024-06-14 22:07:30 -1000

    db

--- Database.java
+++ Database.java
@@ -61,25 +61,5 @@ public class Database {
             englishToSayings.computeIfAbsent(word, k -> new ArrayList<>()).add(s);
         }
     }
-
-    public static void main(String[] args) {
-        Database db = new Database();
-
-        Saying s1 = new Saying("الحب لكل واحد", "Love one another", "شرح باللغة العربية", "Explanation in English");
-        Saying s2 = new Saying("اعتني جيدًا", "Take good care", "شرح باللغة العربية", "Explanation in English");
-        Saying s3 = new Saying("الأرض رئيس؛ الرجل خادمها", "The land is a chief; man is its servant", "شرح باللغة العربية", "Explanation in English");
-
-        db.Insert(s1);
-        db.Insert(s2);
-        db.Insert(s3);
-
-        System.out.println("First: " + db.First().getArabic());
-        System.out.println("Last: " + db.Last().getArabic());
-        System.out.println("Predecessor of s2: " + db.Predecessor(s2).getArabic());
-        System.out.println("Successor of s2: " + db.Successor(s2).getArabic());
-
-        System.out.println("Sayings with 'الحب': " + db.MeHua("الحب"));
-        System.out.println("Sayings with 'Love': " + db.WithWord("Love"));
-    }
 }
 
